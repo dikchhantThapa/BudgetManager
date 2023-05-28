a14e72a94d647b582340e039b2d2f83e5e628b9b
dikchhantThapa <69954970+dikchhantThapa@users.noreply.github.com>
2023-05-28 01:31:10 +0000

    Added code to include all calculations

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Main {
   public static void main(String[] args) {
@@ -6,16 +6,31 @@ class Main {
 
     String input = "";
     String outputUserChoices = "";
-    int total = 0;
+    double total = 0.0;
     
     while (scanner.hasNextLine()) {
       input = scanner.nextLine();
       outputUserChoices += input + "\n";
 
-      // slice up all the numbers after $ and add it to the input
-    }
+      if (input.contains("$")) {
+        // delete $ and convert Strings into multiple strings
+        String[] parts = input.split("\\$");
 
+        // We do int i = 1, since we don't need the first part before $
+        for (int i = 1; i < parts.length; i++) {
+          // Extract the numeric digits and decimal point from the substring 
+          String numberString = parts[i].replaceAll("[^\\d.]+", "");
+          if (!numberString.isEmpty()) {
+            // convert the extracted String into a double
+            double number = Double.parseDouble(numberString);
+            total += number;    // add the number to our total 
+          }
+        } 
+      }
+    }
     
     System.out.println(outputUserChoices);
+    System.out.print("Total: $");
+    System.out.printf("%.2f", total);
   }
 }
