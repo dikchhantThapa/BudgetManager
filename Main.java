358364c0cd8483ddc2fe1486c79439a4d234a6c7
dikchhantThapa <69954970+dikchhantThapa@users.noreply.github.com>
2023-05-31 22:50:13 +0000

    Added menu of options. Menu option 4 and 0 complete

--- Main.java
+++ Main.java
@@ -1,36 +1,76 @@
 import java.util.*;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
 
-    String input = "";
-    String outputUserChoices = "";
-    double total = 0.0;
-    
-    while (scanner.hasNextLine()) {
-      input = scanner.nextLine();
-      outputUserChoices += input + "\n";
+        Scanner scanner = new Scanner(System.in);
 
-      if (input.contains("$")) {
-        // delete $ and convert Strings into multiple strings
-        String[] parts = input.split("\\$");
+        menu();
 
-        // We do int i = 1, since we don't need the first part before $
-        for (int i = 1; i < parts.length; i++) {
-          // Extract the numeric digits and decimal point from the substring 
-          String numberString = parts[i].replaceAll("[^\\d.]+", "");
-          if (!numberString.isEmpty()) {
-            // convert the extracted String into a double
-            double number = Double.parseDouble(numberString);
-            total += number;    // add the number to our total 
-          }
-        } 
-      }
+        System.out.println(purchase());
+
     }
-    
-    System.out.println(outputUserChoices);
-    System.out.print("Total: $");
-    System.out.printf("%.2f", total);
-  }
+
+    public static void menu() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose your action:");
+        System.out.println("1) Add income");
+        System.out.println("2) Add purchase");
+        System.out.println("3) Show list of purchases");
+        System.out.println("4) Balance");
+        System.out.println("0) Exit");
+
+        // read user Pick
+        int userMenuInput = scanner.nextInt();
+        switch (userMenuInput) {
+            case 2:
+                purchase();
+                break;
+            case 0:
+                System.exit(0);
+                break;
+
+        }
+
+
+    }
+
+    public static double purchase () {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = "";
+        String outputUserChoices = "";
+        double total = 0.0;
+
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            outputUserChoices += input + "\n";
+
+            if (input.contains("$")) {
+                // delete $ and convert Strings into multiple strings
+                String[] parts = input.split("\\$");
+
+                // We do int i = 1, since we don't need the first part before $
+                for (int i = 1; i < parts.length; i++) {
+                    // Extract the numeric digits and decimal point from the substring
+                    String numberString = parts[i].replaceAll("[^\\d.]+", "");
+                    if (!numberString.isEmpty()) {
+                        // convert the extracted String into a double
+                        double number = Double.parseDouble(numberString);
+                        total += number;    // add the number to our total
+                    }
+                }
+            }
+        }
+
+        System.out.println(outputUserChoices);
+        System.out.print("Total: $");
+        System.out.printf("%.2f", total);
+
+        return total;
+
+    }
 }
