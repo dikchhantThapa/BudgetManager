5995315cc65576fbd869e91eeff6a15855b73043
dikchhantThapa <69954970+dikchhantThapa@users.noreply.github.com>
2023-06-09 00:34:14 +0000

    1 food purchases and 2 clothes purchases add purchase and show purchase, part 4 show balance

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class Main {
-  
-  private static String outputListOfPurchases = "";
+   
   private static double totalPurchases = 0.0;
   private static double income = 0.0;
+
+  private static List<String> foodPurchases = new ArrayList<>();
+  private static List<String> clothesPurchases = new ArrayList<>();
 
   public static void main(String[] args) {
       menu();
@@ -28,7 +30,7 @@ public class Main {
     switch (userMenuInput) {
       case 1:
         System.out.println("\nEnter income: ");
-        income = scanner.nextDouble();
+        income += scanner.nextDouble();
         System.out.println("Income was added!\n");
         menu();
       case 2:
@@ -36,49 +38,62 @@ public class Main {
         System.out.println("Choose the type of purchase");
         System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) All \n6) Back"); 
         int purchaseInput = scanner.nextInt();
+        
         switch(purchaseInput) {
           case 1:
-            outputListOfPurchases += purchase() + "\n";        
-            TypesOfProducts.FOOD.purchases.add(outputListOfPurchases);
-            System.out.println(TypesOfProducts.FOOD.purchases);
-            map.put(1, TypesOfProducts.FOOD);
-            System.out.println(map);
+            foodPurchases.add(purchase("food"));
+            break;
+          case 2:
+            clothesPurchases.add(purchase("clothes"));
+            break;
           case 6:
             break;
         }
-        // ** need to fix this line later (this is only a variable for output)
         
         menu();
+        
       case 3:
         System.out.println("Choose the type of purchase");
         System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) All \n6) Back"); 
         int outputPurchases = scanner.nextInt(); 
-        for (var val : map.entrySet()) {
-          System.out.println(map.get(outputPurchases));
+        switch(outputPurchases) {
+          case 1:
+            if (foodPurchases.isEmpty()) {
+              System.out.println("The food purchase list is empty\n");
+            }  else {
+              for (String purchase : foodPurchases) {
+                System.out.println(purchase);
+              }
+              System.out.println("Total sum: $" + total(foodPurchases));
+              System.out.println();
+            }
+            break;
+          case 2:
+            if (clothesPurchases.isEmpty()) {
+              System.out.println("The clothes purchase list is empty\n");
+            }  else {
+              for (String purchase : clothesPurchases) {
+                System.out.println(purchase);
+              }
+              System.out.println("Total sum: $" + total(clothesPurchases));
+              System.out.println();
+            }
+            break;
         }
         
-        if (outputListOfPurchases == "") {
-          System.out.println("\nThe purchase list is empty\n");
-        }  else {
-          System.out.println();
-          System.out.print(outputListOfPurchases);
-          totalPurchases = total(outputListOfPurchases);
-          System.out.println("Total sum: $" + totalPurchases);
-          System.out.println();
-        }
         menu();
+        
       case 4:
+        totalPurchases = total(foodPurchases) + total(clothesPurchases);
         System.out.println("\nBalance: $" + (income - totalPurchases) + "\n");
         menu();
       case 0:
         System.out.println("\nBye!");
         System.exit(0);
-        break;
-        
     }
   }
 
-  public static String purchase() {
+  public static String purchase(String purchaseType) {
     Scanner scanner = new Scanner(System.in);
 
     String listOfPurchase = "";
@@ -98,9 +113,10 @@ public class Main {
     
   }
 
-  public static double total(String input) {
+  public static double total(List<String> purchases) {
     double total = 0.0;
-    if (input.contains("$")) {
+
+    for (String input : purchases) {
       // delete $ and convert Strings into multiple strings
       String[] parts = input.split("\\$");
   
@@ -114,10 +130,8 @@ public class Main {
           total += number;    // add the number to our total 
         }
       } 
-    }
-  
-    // System.out.print("Total: $");
-    // System.out.printf("%.2f", total);
+    }    
+
     return total;
   }
   
--- TypesOfProducts.java
+++ TypesOfProducts.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
 public enum TypesOfProducts {
-  FOOD("Food", 0, new ArrayList<String>());
+  FOOD("Food", 0, new ArrayList<String>()),
+  CLOTHES("Clothes", 0, new ArrayList<String>()),
+  ENTERTAINMENT("Entertainment", 0, new ArrayList<String>()),
+  OTHER("Other", 0, new ArrayList<String>());
 
   String name;
   double sum;
