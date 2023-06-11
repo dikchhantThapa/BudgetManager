493004490494cb137d022f9aa5657bf643861dd0
dikchhantThapa <69954970+dikchhantThapa@users.noreply.github.com>
2023-06-11 03:15:53 +0000

    completed all parts + removed redundant code

--- Main.java
+++ Main.java
@@ -18,59 +18,69 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Choose your action:");
+    System.out.println("\nChoose your action:");
     System.out.println("1) Add income");
     System.out.println("2) Add purchase");
     System.out.println("3) Show list of purchases");
     System.out.println("4) Balance");
     System.out.println("0) Exit");
 
-    try {
-      // read user Pick
-      int userMenuInput = scanner.nextInt();
-      switch (userMenuInput) {
-        case 1:
-          System.out.println("\nEnter income: ");
-          income += scanner.nextDouble();
-          System.out.println("Income was added!\n");
-          menu();
-        case 2:
-          // inside user purchase list
+
+    switch (scanner.nextLine()) {
+      case "1":
+        System.out.println("\nEnter income: ");
+        income += scanner.nextDouble();
+        System.out.println("Income was added!\n");
+        menu();
+      case "2":
+        // inside user purchase list
+        System.out.println("\nChoose the type of purchase");
+        System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) Back");
+
+        //   int purchaseInput = scanner.nextInt();
+
+        String purchaseInput = scanner.nextLine();
+
+        while (purchaseInput != "5") {
+
+          switch(purchaseInput) {
+            case "1":
+              foodPurchases.add(purchase());
+              break;
+            case "2":
+              clothesPurchases.add(purchase());
+              break;
+            case "3":
+              entertainmentPurchases.add(purchase());
+              break;
+            case "4":
+              otherPurchases.add(purchase());
+              break;
+            case "5":
+              menu();
+          }
           System.out.println("\nChoose the type of purchase");
           System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) Back");
 
-          int purchaseInput = scanner.nextInt();
+          purchaseInput = scanner.nextLine();
 
-          switch(purchaseInput) {
-            case 1:
-              foodPurchases.add(purchase());
-              break;
-            case 2:
-              clothesPurchases.add(purchase());
-              break;
-            case 3:
-              entertainmentPurchases.add(purchase());
-              break;
-            case 4:
-              otherPurchases.add(purchase());
-              break;
-            case 5:
-              break;
-          }
+        }
 
-          menu();
+      case "3":
+        System.out.println("\nChoose the type of purchases");
+        System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) All \n6) Back");
 
-        case 3:
-          System.out.println("\nChoose the type of purchases");
-          System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) All \n6) Back");
+        String outputPurchases = scanner.nextLine();
+        System.out.println();
 
+        while (outputPurchases != "6") {
 
-          int outputPurchases = scanner.nextInt();
           switch(outputPurchases) {
-            case 1:
+            case "1":
               if (foodPurchases.isEmpty()) {
-                System.out.println("The food purchase list is empty\n");
+                System.out.println("\nThe food purchase list is empty\n");
               }  else {
+                System.out.println();
                 for (String purchase : foodPurchases) {
                   System.out.println(purchase);
                 }
@@ -78,10 +88,11 @@ public class Main {
                 System.out.println();
               }
               break;
-            case 2:
+            case "2":
               if (clothesPurchases.isEmpty()) {
-                System.out.println("The clothes purchase list is empty\n");
+                System.out.println("\nThe clothes purchase list is empty\n");
               }  else {
+                System.out.println();
                 for (String purchase : clothesPurchases) {
                   System.out.println(purchase);
                 }
@@ -89,10 +100,11 @@ public class Main {
                 System.out.println();
               }
               break;
-            case 3:
+            case "3":
               if (entertainmentPurchases.isEmpty()) {
-                System.out.println("The entertainment purchase list is empty\n");
+                System.out.println("\nThe entertainment purchase list is empty\n");
               }  else {
+                System.out.println();
                 for (String purchase : entertainmentPurchases) {
                   System.out.println(purchase);
                 }
@@ -100,10 +112,11 @@ public class Main {
                 System.out.println();
               }
               break;
-            case 4:
+            case "4":
               if (otherPurchases.isEmpty()) {
-                System.out.println("The other purchase list is empty\n");
+                System.out.println("\nThe other purchase list is empty\n");
               }  else {
+                System.out.println();
                 for (String purchase : otherPurchases) {
                   System.out.println(purchase);
                 }
@@ -111,7 +124,7 @@ public class Main {
                 System.out.println();
               }
               break;
-            case 5:
+            case "5":
               System.out.println("\nAll: ");
               for (String purchase : foodPurchases) {
                 System.out.println(purchase);
@@ -130,24 +143,26 @@ public class Main {
               System.out.printf("Total sum: $%.2f", totalPurchases);
               System.out.println("\n");
               break;
+
+            case "6":
+              menu();
+
           }
+          System.out.println("\nChoose the type of purchases");
+          System.out.println("1) Food \n2) Clothes \n3) Entertainment \n4) Other \n5) All \n6) Back");
+          outputPurchases = scanner.nextLine();
 
-          menu();
+        }
 
-        case 4:
-          totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
-          System.out.println("\nBalance: $" + (income - totalPurchases) + "\n");
-          menu();
-        case 0:
-          System.out.println("\nBye!");
-          System.exit(0);
-      }
-    } catch(InputMismatchException e) {
-      System.out.println("\nInvalid input. Please enter a valid option.\n");
-      scanner.nextLine(); // Clear the input buffer
-      menu();
+      case "4":
+        totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
+        System.out.println("\nBalance: $" + (income - totalPurchases) + "\n");
+        menu();
+      case "0":
+        System.out.println("\nBye!");
+        System.exit(0);
+
     }
-
 
   }
 
