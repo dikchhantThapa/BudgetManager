b5338a85bccaa6839afdd384281ece318620c84d
dikchhantThapa <69954970+dikchhantThapa@users.noreply.github.com>
2023-06-21 06:15:53 +0000

    added 6) Load and other options to access it too

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
 
   private static double totalPurchases = 0.0;
   private static double income = 0.0;
+  private static double loadedBalance = 0.0;
 
   private static List<String> foodPurchases = new ArrayList<>();
   private static List<String> clothesPurchases = new ArrayList<>();
@@ -89,8 +90,8 @@ public class Main {
                 for (String purchase : foodPurchases) {
                   System.out.println(purchase);
                 }
-                System.out.println("Total sum: $" + total(foodPurchases));
-                System.out.println();
+                // System.out.println("Total sum: $" + total(foodPurchases));
+                // System.out.println();
               }
               break;
             case "2":
@@ -101,8 +102,8 @@ public class Main {
                 for (String purchase : clothesPurchases) {
                   System.out.println(purchase);
                 }
-                System.out.println("Total sum: $" + total(clothesPurchases));
-                System.out.println();
+                // System.out.println("Total sum: $" + total(clothesPurchases));
+                // System.out.println();
               }
               break;
             case "3":
@@ -113,8 +114,8 @@ public class Main {
                 for (String purchase : entertainmentPurchases) {
                   System.out.println(purchase);
                 }
-                System.out.println("Total sum: $" + total(entertainmentPurchases));
-                System.out.println();
+                // System.out.println("Total sum: $" + total(entertainmentPurchases));
+                // System.out.println();
               }
               break;
             case "4":
@@ -125,8 +126,8 @@ public class Main {
                 for (String purchase : otherPurchases) {
                   System.out.println(purchase);
                 }
-                System.out.println("Total sum: $" + total(otherPurchases));
-                System.out.println();
+                // System.out.println("Total sum: $" + total(otherPurchases));
+                // System.out.println();
               }
               break;
             case "5":
@@ -145,8 +146,8 @@ public class Main {
               }
 
               totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
-              System.out.printf("Total sum: $%.2f", totalPurchases);              
-              System.out.println("\n");
+              System.out.printf("Total sum: $%.2f", totalPurchases);
+//              System.out.println("\n");
 
               break;
 
@@ -161,15 +162,24 @@ public class Main {
         }
 
       case "4":
-        totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
-        System.out.println("\nBalance: $" + (income - totalPurchases) + "\n");
+        if (loadedBalance != 0.0) {
+          System.out.println("\nBalance: $" + loadedBalance);
+        }  else {
+          totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
+          System.out.println("\nBalance: $" + (income - totalPurchases) + "\n");
+        }
         menu();
 
       case "5":
         savePurchasesToFile();
         System.out.println("\nPurchases were saved!");
         menu();
-        
+
+      case "6":
+        loadPurchasesFromFile();
+        System.out.println("\nPurchases were loaded!");
+        menu();
+
       case "0":
         System.out.println("\nBye!");
         System.exit(0);
@@ -203,20 +213,32 @@ public class Main {
 
     for (String input : purchases) {
       // delete $ and convert Strings into multiple strings
-      String[] parts = input.split("\\$");
+//      String[] parts = input.split("\\$");
+      int lastIndex = input.lastIndexOf("$");
+      String numberString = input.substring(lastIndex + 1);
 
-      // We do int i = 1, since we don't need the first part before $
-      for (int i = 1; i < parts.length; i++) {
-        // Extract the numeric digits and decimal point from the substring
-        String numberString = parts[i].replaceAll("[^\\d.]+", "");
+      if (!numberString.isEmpty()) {
+        // extract numeric digits and decimal from the substring
+        numberString = numberString.replaceAll("[^\\d.]+", "");
         if (!numberString.isEmpty()) {
-          // convert the extracted String into a double
+          // convert extracted String into a double
           double number = Double.parseDouble(numberString);
-          total += number;    // add the number to our total
+          total += number;  // add the number to our total
         }
       }
-    }
 
+      // We do int i = 1, since we don't need the first part before $
+//      for (int i = 1; i < parts.length; i++) {
+//        // Extract the numeric digits and decimal point from the substring
+//        String numberString = parts[i].replaceAll("[^\\d.]+", "");
+//        if (!numberString.isEmpty()) {
+//          // convert the extracted String into a double
+//          double number = Double.parseDouble(numberString);
+//          total += number;    // add the number to our total
+//        }
+//      }
+    }
+    total = Math.round(total * 100.0)/100.0;
     return total;
   }
 
@@ -227,34 +249,96 @@ public class Main {
 
       writer.println("All Purchases: ");
 
-      // writer.println("Food purchases: ");
+      writer.println("Food purchases: ");
       for (String purchase : foodPurchases) {
         writer.println(purchase);
       }
 
-      // writer.println("\nClothes purchases: ");
+      writer.println("Clothes purchases: ");
       for (String purchase : clothesPurchases) {
         writer.println(purchase);
       }
 
-      // writer.println("\nEntertainment purchases: ");
+      writer.println("Entertainment purchases: ");
       for (String purchase : entertainmentPurchases) {
         writer.println(purchase);
       }
 
-      // writer.println("\nOther purchases: ");
+      writer.println("Other purchases: ");
       for (String purchase : otherPurchases) {
         writer.println(purchase);
       }
-      
+
       totalPurchases = total(foodPurchases) + total(clothesPurchases) + total(entertainmentPurchases) + total(otherPurchases);
-      writer.println("\nTotal sum: $" + totalPurchases);
-      
-      
+      //writer.println("\nTotal sum: $" + totalPurchases);
+
+      writer.println("\nBalance: $" + (income - totalPurchases) + "\n");
+
       writer.close();
     }  catch (IOException e) {
-      System.out.println("Error saving purchases to file"); 
+      System.out.println("Error saving purchases to file");
     }
+  }
+
+  public static void loadPurchasesFromFile() {
+    try {
+      File file = new File("purchases.txt");
+      Scanner scanner = new Scanner(file);
+
+      // Clear existing purchase lists
+      foodPurchases.clear();
+      clothesPurchases.clear();
+      entertainmentPurchases.clear();
+      otherPurchases.clear();
+
+      String currentCategory = null; // Track the current category being processed
+
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+
+        // Skip empty lines or lines starting with a specific label
+        // if (line.isEmpty() || line.startsWith("All Purchases:") || line.startsWith("Total sum:") || line.startsWith("Balance:")) {
+        //   continue;
+        // }
+
+        // Check if the line indicates a category
+        if (line.startsWith("Food")) {
+          currentCategory = "Food";
+        } else if (line.startsWith("Clothes")) {
+          currentCategory = "Clothes";
+        } else if (line.startsWith("Entertainment")) {
+          currentCategory = "Entertainment";
+        } else if (line.startsWith("Other")) {
+          currentCategory = "Other";
+        } else if (line.startsWith("Balance")) {
+          // saving the loaded balance from file to the class level variable "loadedBalance"
+          String balanceString = line.substring(line.indexOf("$") + 1).trim();
+          loadedBalance = Double.parseDouble(balanceString);
+        }
+
+        else if (currentCategory != null) {
+          // Add the line to the respective list based on the current category
+          switch (currentCategory) {
+            case "Food":
+              foodPurchases.add(line.trim());
+              break;
+            case "Clothes":
+              clothesPurchases.add(line.trim());
+              break;
+            case "Entertainment":
+              entertainmentPurchases.add(line.trim());
+              break;
+            case "Other":
+              otherPurchases.add(line.trim());
+              break;
+          }
+        }
+      }
+      scanner.close();
+    } catch (IOException e) {
+      System.out.println("Error loading purchases from file");
+    }
+
   }
 
 }
